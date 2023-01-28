32197321ef4cc07bd6d9f395eab63e5502e60205
antares <antares@cipherbiz.com>
2023-01-29 00:06:25 +0300

    change mongoDb url add Model MovieSchema

--- helper/db.js
+++ helper/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 module.exports=() =>{
 
-    mongoose.connect('mongodb+srv://ahmetemn:ahmetgs123A@apimovie.ivag3pe.mongodb.net/test')
+  mongoose.connect("mongodb+srv://ahmemn1:ahmetgs123A@movieapp1.74r9g1m.mongodb.net/test")
 
     mongoose.connection.on('open' , () =>{
         console.log('MongoDb Connected')
